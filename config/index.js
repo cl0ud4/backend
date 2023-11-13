d04e0791ffc456fa966b874440086ba0ea1bbdb4
yotak <tech.yuje@gmail.com>
2023-11-13 10:01:21 +0900

    🔥hofix: remove config/index.js

--- config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const express = require("./config/express");
-
-const port = 4000;
-express().listen(port);
-const env = process.env.NODE_ENV || "development";
-console.log(`${env} - API Server Start At Port ${port}`);
