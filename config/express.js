e4ce508e1082540db5d5b91b5a35fb39c4af10e8
listenyoon <listenyoon@gmail.com>
2023-12-12 08:49:01 +0000

    mod: move app.get location

--- config/express.js
+++ config/express.js
@@ -26,5 +26,15 @@ module.exports = function () {
   // require("../src/app/Test/testRoute")(app);
   require("../src/app/Question/questionRoute")(app);
 
+  app.get('/healthz', function(request, response) {
+	  console.log("in health");
+	  response.status(200).send('OK').end();
+  });
+
+  app.get('/ready', function(request, response) {
+	  console.log("in ready");
+	  response.status(200).send('OK').end();
+  });
+
   return app;
 };
